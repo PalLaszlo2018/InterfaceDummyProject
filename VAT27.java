3af71e83f9e8c190da33625e880d948fd1482022
PalLaszlo2018 <45828825+PalLaszlo2018@users.noreply.github.com>
2020-03-20 05:12:08 +0100

    Add files via upload

---
+++ VAT27.java
@@ -0,0 +1,17 @@
+public interface VAT27 {
+
+    int VAT = 27; // public static -ként működik implicit módon
+
+    static void printActualVAT() { // body nem kötelező, de lehet
+        // System.out.println(VAT);
+    }
+
+    default double calculateDoubleVAT(int vatRate) { // body nem kötelező, de lehet
+        double doubleRate = 0;
+        doubleRate = (double) vatRate / 100;
+        return doubleRate;
+    }
+
+
+    double calculateFullPrice(int net); // ez public abstract implicit módon, nincs body
+}
